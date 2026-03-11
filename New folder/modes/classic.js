17c5a7316dd7b45613c396334d1df7e5552d851a
MadMonkeyNZ <132816269+MadMonkeyNZ@users.noreply.github.com>
2026-03-12 08:20:34 +1300

    Delete New folder directory

--- New folder/modes/classic.js
+++
@@ -1,5 +0,0 @@
-/* ================= CLASSIC MODE ================= */
-
-// Classic mode uses the default startGame function from game-core.js
-// No additional functions needed for basic classic mode
-
